b2cb4d7504aa977b80e6926214597bf72995d838
Manqoba Hlongwane <mmanqoba866@gmail.com>
2025-10-05 19:38:35 +0200

    Add hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+input("Enter your name: ")
+name = input()
+print(f"Hello, {name}!")
+# This code prompts the user to enter their name and then greets them 
+# The variable 'name' stores what the user entered into the box 
+
+input("Enter your age: ")
+age= input()
+print(f"You are {age} years old.")
+# This code prompts the user to enter their age
+# The variable 'age' also stores what the user entered into the box
+
+print("Hello, World!") 
+# This code prints "Hello, World!" to the console.
+# "Hello, World!" is displayed in the output. This is because "Hello, World!" is a string 
